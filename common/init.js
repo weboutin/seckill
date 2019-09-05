08f25f8cdd5d52df3b4c049c0749b8918ff5f5fc
itgo <634872896@qq.com>
2019-09-05 23:53:01 +0800

    feat: add refresh redis in init script

--- common/init.js
+++ common/init.js
@@ -1,10 +1,15 @@
 const Product = require('./models/product');
 const Order = require('./models/order');
+const RedisClient = require('./getRedisInstance');
+const { KEY_ORDERS } = require('./models/rediskey');
 
 async function init(stock) {
   //refresh
   await Order.sync({ force: true })
   await Product.sync({ force: true })
+
+  await RedisClient.delAsync(KEY_ORDERS);
+
   //init data
   await Product.create({
     title: '代购买商品',
