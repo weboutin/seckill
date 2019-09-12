c361687e26c9151582b4ea70de281852464d557b
itgo <634872896@qq.com>
2019-09-12 22:35:17 +0800

    feat: check affect row to increase mysql query count

--- v3/index.js
+++ v3/index.js
@@ -1,29 +1,34 @@
 const Product = require('../common/models/product');
 const RedisClient = require('../common/getRedisInstance')
 const sequelize = require('../common/getInstance');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const { KEY_ORDERS } = require("../common/models/rediskey")
 
 module.exports = async (uid, productId, stock) => {
   let transaction;
   try {
     transaction = await sequelize.transaction();
-    await Product.decrement('stock', {
+    let rs = await Product.decrement('stock', {
       where: {
-        id: productId
+        id: productId,
+        stock: {
+          [Op.gt]: 0
+        }
       },
       transaction
     })
-    let productInfo = await Product.findOne({ productId: productId, transaction })
-    if (productInfo.toJSON().stock < 0) {
+    if (rs[0][1] == 0) {
       throw new Error('stock not enough')
     }
     await transaction.commit();
+    await RedisClient.lpushAsync(KEY_ORDERS, `uid:${uid}|productId:${productId}`);
   } catch (err) {
     if (err) {
+      console.log(err)
       await transaction.rollback();
       return false;
     }
   }
-  await RedisClient.lpushAsync(KEY_ORDERS, `uid:${uid}|productId:${productId}`);
   return true;
 }
